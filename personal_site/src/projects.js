68cbc93b6ff1f459c66e79d65dfadd55eb5158d4
bigriceman0128 <112298940+ardenmonaghan@users.noreply.github.com>
2025-03-13 20:34:41 -0600

    changes to projects

--- personal_site/src/projects.js
+++ personal_site/src/projects.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import scienceImg from './images/science.png';
 import posImg from './images/pos.png';
-import localParseImg from './images/background.jpg';
 import portfolioImg from "./images/portfolio.png"
 import neuralNetworkImg from "./images/neural_network.png"
 import { motion } from 'framer-motion';
 import { fadeIn, opacity } from './utils/motion';
+import moodifyImg from './images/moodify.png'
 
 
 
@@ -41,34 +41,34 @@ const Projects = () => {
       id: 1,
       name: 'MentorMatch UAlberta',
       time: 'April 2024 - Sept 2024',
-      description: 'MentorMatch Ualberta is a Software that utilizes Natural Language Processing Models to help students in the Science Mentorship Program at UAlberta be matched with mentors. Optimizing the time significantly for Program Coordinators and Mentors.',
+      description: 'MentorMatch UAlberta is a Software that utilizes NLP Models to help students in the Science Mentorship Program at UAlberta be matched with mentors. Optimizing the time significantly for Program Coordinators and Mentors.',
       link: 'https://github.com/MentorMatch1',
-      tags: ["Python", "PyQT", "Ollama", "NLP", "Docker"],
+      tags: ["Python", "PyQt6", "Ollama", "NLP", "Docker"],
       image: scienceImg
     },
     {
       id: 2,
+      name: 'Moodify',
+      time:'Jan 2025 - Mar 2025',
+      description: 'Moodify is a full stack Android application that follows a Twitter style design. Some features include a user following system, a user feed for individuals you currently follow, and geolocation based moods to see how people are feeling in your area. CMPUT301 Winter 2025 Group Project.',
+      link: 'https://github.com/ardenmonaghan/',
+      tags: ["Android Studio", "Java", "Firebase", "Git"],
+      image: moodifyImg
+    },
+    {
+      id: 3,
       name: 'NHL Positivity Index',
       time: 'Sept 2023 - Jan 2024',
       description: "This project involved conducting sentiment analysis on data from the NHL subreddit to measure the positivity associated with each of the 32 NHL teams by fine-tuning the cardiffnlp/twitter-roberta-base-sentiment-latest model specifically on hockey-related sentiment data.",
       link: 'https://uais.dev/projects/nhl-positivity-index/',
       image: posImg,
-      tags: ["Python", "HuggingFace", "Sentiment Analysis", "PRAW", "SeaBorn"]
-    },
-    {
-      id: 3,
-      name: 'LocalParse',
-      time:'Sept 2024 - Present',
-      description: 'LocalParse is a software that Utilizes LLMs to parse through large documents and quickly retrieve + summarize information without compromising on privacy. Uses Downloadable Software GUI Interface for Easy Access and Usability',
-      link: 'https://github.com/ardenmonaghan/LocalParse-ML',
-      tags: ["Python", "Ollama", "Langchain", "PyQt6", "SQLite"],
-      image: localParseImg
-    },
+      tags: ["Python", "HuggingFace", "Sentiment Analysis", "PRAW", "Matplotlib"]
+    },  
     {
       id: 4,
       name: 'Scratch Neural Network',
       time:'December 2024',
-      description: 'This is a neural network that I built from scratch only using Numpy and Linear Algebra to perform forward propagation and backpropagation. It explains the fundementals of how neural networks work and how I was able to implement it on the MNIST dataset for classifying images 1-9 The model achieves >90% Accuracy on 50 Epochs. Additionally it includes other implementations of Neural Networks. Including using a CNN via PyTorch to classify images of many different objects',
+      description: 'This is a neural network that I built from scratch only using Numpy and Linear Algebra to perform forward propagation and backpropagation. It explains the fundementals of how neural networks work and how I was able to implement it on the MNIST dataset for classifying images 1-9 The model achieves >90% Accuracy on 50 Epochs. Additionally it includes a CNN via PyTorch to classify images of many different objects.',
       link: 'https://github.com/ardenmonaghan/Neural_Network_Test',
       tags: ["Pytorch", "CNNs", "Neural Networks", "Machine Learning", "Python", "Numpy", "Matplotlib"],
       image: neuralNetworkImg
@@ -77,7 +77,7 @@ const Projects = () => {
       id: 5,
       name: 'Portfolio Website',
       time:'August 2024 - Present',
-      description: 'This website is a portfolio of my work and a showcase of my skills. It is built using React, Framer Motion, and CSS. It is hosted on Vercel.',
+      description: 'This website is a portfolio of my work and a showcase of my skills. It is built using React, JavaScript, CSS. It is hosted on Vercel.',
       link: 'https://github.com/ardenmonaghan/personalSite',
       tags: ["HTML", "CSS", "JavaScript", "ReactJS", "Vercel"],
       image: portfolioImg
